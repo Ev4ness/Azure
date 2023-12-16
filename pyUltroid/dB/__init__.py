3c35fbe965bebfb19d79e93e6c1e66afee4135c6
Vooyage <148488760+Vygaee@users.noreply.github.com>
2023-12-16 19:20:10 +0700

    Update __init__.py

--- pyUltroid/dB/__init__.py
+++ pyUltroid/dB/__init__.py
@@ -10,13 +10,7 @@ if not run_as_module:
 from .. import *
 
 DEVLIST = [
-    719195224,  # @xditya
-    1322549723,  # @danish_00
-    1903729401,  # @its_buddhhu
-    1303895686,  # @Sipak_OP
-    611816596,  # @Arnab431
-    1318486004,  # @sppidy
-    803243487,  # @hellboi_atul
+   835478438 ,  # @CybrUseriT
 ]
 
 ULTROID_IMAGES = [
