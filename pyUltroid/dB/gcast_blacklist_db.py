3a8eb1d8bbc6e1138f33d8be1bbac2eec91226d9
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-02-19 16:27:17 +0700

    Add files via upload

---
+++ pyUltroid/dB/gcast_blacklist_db.py
@@ -0,0 +1,33 @@
+from .. import udB
+
+
+def get_stuff():
+    return udB.get_key("GBLACKLISTS") or []
+
+
+def add_gblacklist(id):
+    ok = get_stuff()
+    if id not in ok:
+        ok.append(id)
+        return udB.set_key("GBLACKLISTS", ok)
+
+
+def rem_gblacklist(id):
+    ok = get_stuff()
+    if id in ok:
+        ok.remove(id)
+        return udB.set_key("GBLACKLISTS", ok)
+
+
+def is_gblacklisted(id):
+    return id in get_stuff()
+
+
+def black_aja():
+    return udB.get_key("GBLACKLISTS") or {}
+
+
+def list_bl(id):
+    ok = black_aja()
+    for id in ok:
+        return "".join(f"**๏** `{z}`\n" for z in ok)
