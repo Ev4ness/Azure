22856446959fd7afd699fafaffa89cc5491ae1c9
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-03-07 14:44:24 +0700

    Update __init__.py

--- pyUltroid/__init__.py
+++ pyUltroid/__init__.py
@@ -99,7 +99,7 @@ if run_as_module:
     DUAL_HNDLR = udB.get_key("DUAL_HNDLR") or "/"
     SUDO_HNDLR = udB.get_key("SUDO_HNDLR") or HNDLR
 else:
-    print("pyUltroid 2022 © TeamUltroid")
+    print("dantUBot 2024\n@MusicStreamSupport")
 
     from logging import getLogger
 
