c1f1390107c9066ae1b99e248cf6c62e4e69343d
Vooyage <148488760+Vygaee@users.noreply.github.com>
2024-03-07 10:18:45 +0700

    Update Salam.py

--- plugins/Salam.py
+++ plugins/Salam.py
@@ -30,50 +30,50 @@ Perintah Tersedia
 from time import sleep
 from . import (
     eor,
-    dante_cmd,
+    ultroid_cmd,
 )
 
-@dante_cmd(pattern="ass$")
+@ultroid_cmd(pattern="ass$")
 async def _(event):
     await event.eor("**Assalamu'alaikum Warohmatulohi Wabarokatu**")
 
 
-@dante_cmd(pattern="as$")
+@ultroid_cmd(pattern="as$")
 async def _(event):
     await event.eor("**Assalamu'alaikum**")
     
-@dante_cmd(pattern="ws$")
+@ultroid_cmd(pattern="ws$")
 async def _(event):
     await event.eor("**Wa'alaikumussalam**")
 
     
-@dante_cmd(pattern="ks$")
+@ultroid_cmd(pattern="ks$")
 async def _(event):
     xx = await event.eor("**Hy kaa 🥹**")
     sleep(2)
     await xx.edit("**Assalamualaikum...**")
 
 
-@dante_cmd(pattern="jws$")
+@ultroid_cmd(pattern="jws$")
 async def _(event):
     xx = await event.eor(event, "**Astaghfirullah, Jawab dulu salam dong**")
     sleep(2)
     await xx.edit("**Assalamu'alaikum**")
 
 
-@dante_cmd(pattern="3x$")
+@ultroid_cmd(pattern="3x$")
 async def _(event):
     xx = await event.eor("**Bismillah, 3x**")
     sleep(2)
     await xx.edit("**Assalamu'alaikum Bisa yug Kali**")
     
-@dante_cmd(pattern="kg$")
+@ultroid_cmd(pattern="kg$")
 async def _(event):
     xx = await event.eor("**Nih Gw Pantun,Buah Apel Buah Kedondong**")
     sleep(2)
     await xx.edit("**Senggol Dong!!!**")
 
-@dante_cmd(pattern="hm$")
+@ultroid_cmd(pattern="hm$")
 async def _(event):
     xx = await event.eor("**Batuk dulu g sih**")
     sleep(2)
